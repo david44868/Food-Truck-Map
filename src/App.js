bf0c9f02a1c3b9a8b8235e4d5f5874754a8941da
David <davidhariantowu@gmail.com>
2023-05-19 18:12:28 -0400

    Search bar feature

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import L from 'leaflet';
 function App() {
 
   const [trucks, setTrucks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredTrucks, setFilteredTrucks] = useState(trucks);
   const greenIcon = L.icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -33,11 +35,21 @@ function App() {
     shadowSize: [41, 41]
   });
 
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+  
+    const filtered = trucks.filter((truck) =>
+      truck.applicant.toLowerCase().includes(query)
+    );
+    setFilteredTrucks(filtered);
+  };
+
   useEffect(() => {
     fetch(`https://food-truck-map.onrender.com/food_trucks`, {
       mode: 'cors',
       headers: {
-        'Access-Control-Allow-Origin': 'https://food-truck-map-five.vercel.app'
+        'Access-Control-Allow-Origin': 'https://food-truck-map-five.vercel.app/'
       }
     })
       .then(response => response.json())
@@ -50,16 +62,19 @@ function App() {
 
   return (
     <div>
-      {/* <input id="search" 
-        placeholder="Enter the restaurant name..."
-        autoComplete="off">
-      </input> */}
+      <input id="search" 
+        placeholder="Enter the food truck name..."
+        autoComplete="off"
+        value={searchQuery}
+        onChange={handleSearch}
+      >
+      </input>
       <MapContainer style={{ height: "100vh", width: "100%" }} center={[37.773972, -122.431297]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {trucks.map((truck, index) => {
+        {searchQuery === '' ? (trucks.map((truck, index) => {
           let markerIcon, name;
           if (truck.status === 'APPROVED') {
             markerIcon = greenIcon;
@@ -81,7 +96,29 @@ function App() {
                 <span class="popup">Menu: </span>{truck.food_items}
               </Popup>
             </Marker>
-        )})}
+        )})) : (filteredTrucks.map((truck, index) => {
+          let markerIcon, name;
+          if (truck.status === 'APPROVED') {
+            markerIcon = greenIcon;
+            name = "green";
+          } else if (truck.status === 'REQUESTED') {
+            markerIcon = yellowIcon;
+            name = "yellow";
+          } else {
+            markerIcon = redIcon;
+            name = "red";
+          }
+          
+          return (
+            <Marker position={[truck.location.latitude, truck.location.longitude]} icon={markerIcon}>
+              <Popup>
+                <span class="popup">{truck.applicant}</span> <br/>
+                <span class="popup">Address: </span>{truck.address} <br/>
+                <span class="popup">Status: </span><span class={name}>{truck.status}</span> <br/>
+                <span class="popup">Menu: </span>{truck.food_items}
+              </Popup>
+            </Marker>
+        )}))}
         
       </MapContainer>
     </div>
